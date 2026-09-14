71c59b12d5b820f2bd122e4de0726bb0488588f8
kevinpita <69063372+kevinpita@users.noreply.github.com>
2026-09-14 02:31:59 +0000

    chore: update orca to version 1.4.202

--- package.nix
+++ package.nix
@@ -10,16 +10,16 @@
 
 let
   pname = "orca";
-  version = "1.4.201";
+  version = "1.4.202";
 
   sources = {
     x86_64-linux = {
       url = "https://github.com/stablyai/orca/releases/download/v${version}/orca-linux.AppImage";
-      hash = "sha512-Ho/kVmII0RUpE++JHw4sR0bwAwBPgy7UFd9sDrLi3F/FG4dWgutPM4hsgGbM99YlIudZv1sJwgllBOapmOtysw==";
+      hash = "sha512-Hd5VLh/GW8uvxicMwy/qpY8x7ZZIVpW++2uwvYN2zpNhVH5RgTl+YXc8pqQOSqHQJXZUedeJKaLumnAqJvWT5g==";
     };
     aarch64-linux = {
       url = "https://github.com/stablyai/orca/releases/download/v${version}/orca-linux-arm64.AppImage";
-      hash = "sha512-jpfnY4nTVjdZnoRfS5E97nqXtg0Dy3AewO4KrGd93h1uKGwnwP4OL85hqEBBk3iiz2oXYumeJhMl7X99D6Jx3w==";
+      hash = "sha512-OvsXqBbP0x0fjz+gI9+oOvSJBQvR48y/qYJClu5WYEaTiUn2yucesh/J5M801EumMXSCuXGBTrCSu6BknFqndQ==";
     };
   };
 
